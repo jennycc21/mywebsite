5d32d9aa8b8e08b99b9d78a2655b0ef8a196ac7f
jennycc21 <jennywangcc@gmail.com>
2020-08-21 13:35:42 +1000

    Updates

---
+++ precache-manifest.f83c0dc438d48d09ea4bd42b2ed5275e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f33b33573ffbbc7da7f486dc6b00960f",
+    "url": "/mywebsite/index.html"
+  },
+  {
+    "revision": "87a050dcd7923bd368f1",
+    "url": "/mywebsite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/mywebsite/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mywebsite/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87a050dcd7923bd368f1",
+    "url": "/mywebsite/static/js/main.03ba80ef.chunk.js"
+  },
+  {
+    "revision": "27cdc9c18f62965d4573",
+    "url": "/mywebsite/static/js/runtime-main.d6e467e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mywebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
